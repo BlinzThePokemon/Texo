c8984227c3595a6f37c80e62aa843a7054fb45e7
Binz <yakshgoel090@gmail.com>
2025-04-20 20:07:18 +0530

    Fixed syntax error

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,7 @@ import { getAnalytics } from 'firebase/analytics';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY, ,
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
